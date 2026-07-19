0913b08217060654ba6d7eb5a1b1e593fd8a09dc
Anand Nadar <anandnadar@012.com>
2026-07-20 00:01:20 +0530

    Completed first two Java programs

---
+++ src/variables/MyDetails.java
@@ -0,0 +1,10 @@
+package variables;
+
+public class MyDetails {
+    public static void main(String[] args) {
+        System.out.println("Name: Anand Nadar");
+        System.out.println("Age: 20");
+        System.out.println("City: Malad, Mumbai");
+        System.out.println("Hobby: Solving puzzles");
+    }
+}
