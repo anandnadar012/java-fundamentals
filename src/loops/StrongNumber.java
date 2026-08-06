54db93f3fb1481777c17752c64153885233817d0
Anand Nadar <anandnadar012@gmail.com>
2026-08-06 23:42:55 +0530

    Add Strong number

--- src/loops/StrongNumber.java
+++ src/loops/StrongNumber.java
@@ -2,30 +2,31 @@ package loops;
 import java.util.Scanner;
 
 public class StrongNumber {
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number to checks if its Strong number : ");
+
+        System.out.print("Enter a number to check if it is a Strong number: ");
         int n = sc.nextInt();
-        int fact = 1;
-        int temp = 1;
+        int original = n;
+        int sum = 0;
 
         while (n != 0) {
             int lastDigit = n % 10;
-             temp = temp * 10 + lastDigit;
-            while (temp > 0) {
-                fact = fact * temp;
-                temp--;
+            int fact = 1;
+            for (int i = 1; i <= lastDigit; i++) {
+                fact = fact * i;
             }
+            sum += fact;
             n = n / 10;
         }
 
-        if (fact == n) {
-            System.out.println("Perfect number");
+        System.out.println("Sum of digit factorials: " + sum);
+        if (sum == original) {
+            System.out.println(original + " is a Strong number");
         } else {
-            System.out.println("Not a perfect number");
+            System.out.println(original + " is not a Strong number");
         }
-        
+
         sc.close();
     }
 }
